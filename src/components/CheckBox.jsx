8288354bbac40f7f936abb8f8e91be92d6df55e4
AlvaroJorge <alv.jor96@gmail.com>
2022-06-14 19:07:47 -0300

    Added FormGroup

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,28 +1,30 @@
 import React, {useState} from 'react';
-import { Box, Checkbox, FormControlLabel } from '@mui/material';
+import { Box, Checkbox, FormControlLabel, FormGroup } from '@mui/material';
 
 const CheckBox = () => {
   return (
     <>
         <Box sx={{padding: '1rem'}}>
-            <FormControlLabel
-                label='Bateas Ultrasonido'
-                control={<Checkbox />}/>
-            <FormControlLabel 
-                label='Diagnostico Directo'
-                control={<Checkbox />}/>
-            <FormControlLabel 
-                label='Electrónica / Portones automáticos'
-                control={<Checkbox />}/>
-            <FormControlLabel 
-                label='Escáneres'
-                control={<Checkbox />}/>
-            <FormControlLabel 
-                label='Escáneres Profesionales AUTEL'
-                control={<Checkbox />}/>
-            <FormControlLabel 
-                label='GNC'
-                control={<Checkbox />}/>
+            <FormGroup>
+                <FormControlLabel
+                    label='Bateas Ultrasonido'
+                    control={<Checkbox />}/>
+                <FormControlLabel 
+                    label='Diagnostico Directo'
+                    control={<Checkbox />}/>
+                <FormControlLabel 
+                    label='Electrónica / Portones automáticos'
+                    control={<Checkbox />}/>
+                <FormControlLabel 
+                    label='Escáneres'
+                    control={<Checkbox />}/>
+                <FormControlLabel 
+                    label='Escáneres Profesionales AUTEL'
+                    control={<Checkbox />}/>
+                <FormControlLabel 
+                    label='GNC'
+                    control={<Checkbox />}/>
+            </FormGroup>
         </Box>
     </>
   )
