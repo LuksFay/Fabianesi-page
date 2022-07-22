926baf6c0e70174d6f98b18de7014f9fd63bcbb9
LuksFay <lukspicco@hotmail.com>
2022-07-22 01:56:16 -0300

    a

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -4,11 +4,11 @@ import InfoIcon from '@mui/icons-material/Info';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom';
 
-const Card = (props, {handleAddProduct}) => {
+const Card = ({ items, handleAddProduct }) => {
   return (
     <>
         <div className='card__section'>
-            {props.items.map(item => {
+            {items.map(item => {
                 return(
                 <div className="product__card" key={item.id}>
                     <div className="product__tumb">
@@ -21,7 +21,7 @@ const Card = (props, {handleAddProduct}) => {
                             <div className="product__price">{'$ ' + item.price + '.00'}</div>
                             <div className="product__links">
                                 <Link to={'/' + item.id}><InfoIcon /></Link>
-                                <span onClick={()=>{handleAddProduct(item)}}><ShoppingCartIcon/></span>
+                                <span onClick={()=>handleAddProduct(item)}><ShoppingCartIcon/></span>
                             </div>
                         </div>
                     </div>
