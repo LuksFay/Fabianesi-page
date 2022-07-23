1cc7d17cf0ebf7dffb22ddc820f486ca9a7636b5
luksfay <luksfaydev@gmail.com>
2022-07-23 15:43:44 -0300

    In the end I gave functionality to the sliders. I was looking at the manifest to update it and gave some basic styles to the information sections

--- src/pages/Condiciones.jsx
+++ src/pages/Condiciones.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-//import '../styles/Inicio.css';
+import '../styles/Condiciones.css';
 
 const Condiciones = () => {
   return (
     <>
     <section className='condiciones-section'>
         <div className='condiciones-div-principal'>
-            <h3 className='condiciones-titulo'>¿Cómo es el proceso de compra?</h3>
+            <h2 className='condiciones-titulo'>
+              ¿Cómo es el proceso de compra?
+            </h2>
             <p className='condiciones-descripcion'>
-              Comprar en nuestra tienda es muy sencillo. Solo tienes que añadir a tu carrito de compras los productos que quieras comprar y, una vez que hayas terminado, proceder a realizar la compra. <br />
+              Comprar en nuestra tienda es muy sencillo. Solo tenes que añadir a tu carrito de compras los productos que quieras comprar y, una vez que hayas terminado, proceder a realizar la compra. <br />
               Al hacerlo, se te redirigirá al WhatsApp directo del vendedor, donde vas a poder acordar el medio de pago y la entrega de los productos. <br />
               Nuestra tienda ofrece una gran variedad de productos a precios muy competitivos. Todos nuestros productos son de alta calidad y están garantizados. Además, si no estás satisfech@ con algún producto, podes devolverlo sin problema y te devolveremos el importe. <br />
               En nuestra tienda, la satisfacción del cliente es nuestra principal prioridad. Por eso, si tenes cualquier duda o problema, no dudes en contactar con nosotros y estaremos encantados de ayudarte.
--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import '../styles/Inicio.css';
+import '../styles/Contacto.css';
 
 const Contacto = () => {
   return (
